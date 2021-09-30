ceb89ad57d1584a28a987f5e20fef93df46a0f46
Feng Jiayi <mua6001@163.com>
2021-09-30 09:28:34 -0400

    ajoute images profs

---
+++ theme-codeFibonacci/template-parts/content-prof.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Template part pour l'affichage des images profs dans la page d'accueil 
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package equipe-5
+ */
+
+?>
+<div class="accueil-prof">
+    <h1>Prof</h1>
+    <div class="accueil-img-prof">
+        <div class="prof-1">
+        <img src="wp-content/uploads/photo-prof/Prof-2.jpg">
+        </div>
+        <div class="prof-2">
+        <img src="wp-content/uploads/photo-prof/Prof-3.jpg">
+        </div>
+        <div class="prof-3">
+        <img src="wp-content/uploads/photo-prof/Prof-4.jpg">
+        </div>
+        <div class="prof-4">
+        <img src="wp-content/uploads/photo-prof/Prof-5.jpg">
+        </div>
+        <div class="prof-5">
+        <img src="wp-content/uploads/photo-prof/Prof-6.jpg">
+        </div>
+        <div class="prof-6">
+        <img src="wp-content/uploads/photo-prof/Prof-7.jpg">
+        </div>
+        <div class="prof-7">
+        <img src="wp-content/uploads/photo-prof/Prof-8.jpg">
+        </div>
+        <div class="prof-8">
+        <img src="wp-content/uploads/photo-prof/Prof-9.jpg">
+        </div>
+        <div class="prof-9">
+        <img src="wp-content/uploads/photo-prof/Prof-10.png">
+        </div>
+        <div class="prof-10">
+        <img src="wp-content/uploads/photo-prof/Prof-11.jpg">
+        </div>
+        <div class="prof-11">
+        <img src="wp-content/uploads/photo-prof/Prof-12.png">
+        </div>
+        <div class="prof-12">
+        <img src="wp-content/uploads/photo-prof/Manon.jpg">
+        </div>
+    </div>
