1efec723676e09688601f903e4e71814f1ec5e90
Sami El Arif <75094123+selarif@users.noreply.github.com>
2021-10-03 11:38:05 -0400

    Template part sur front-page

--- theme-codeFibonacci/header.php
+++ theme-codeFibonacci/header.php
@@ -72,6 +72,9 @@
 		<img src="wp-content/uploads/2021/09/background-1.svg" alt="">
 		
 		<?php
-			get_template_part( 'template-parts/content', 'cours' );
+			get_template_part( 'template-parts/content', 'prof' );
 		?> 
+		
+		
+		
 	</header><!-- #masthead -->
