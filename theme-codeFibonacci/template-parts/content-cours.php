47488df73370dfdf69e51494c82ed4de9dc56317
JeremyEmondLapierre <72357541+JeremyEmondLapierre@users.noreply.github.com>
2021-10-28 10:42:13 -0400

    Modification et exemple d'un affichage dynamique avec une boucle while

--- theme-codeFibonacci/template-parts/content-cours.php
+++ theme-codeFibonacci/template-parts/content-cours.php
@@ -9,124 +9,6 @@
 
 ?>
 
-<!-- Affichage statique
-
-<div class="accueil-cours">
-    <h1>Nos cours</h1>
-    <div class="accueil-cours-contenant">
-        <div class="accueil-cours-gauche">
-            <div class="cours-contenu">
-                <div class="accueil-cours-texte">
-                    <h2>Web</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-sphere">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-sphere"></use>
-                    </svg>
-                </a>
-            </div>
-        </div>
-        <div class="accueil-cours-droite">
-            <div class="cours-contenu">
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-film">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-film"></use>
-                    </svg>
-                </a>
-                <div class="accueil-cours-texte">
-                    <h2>Création Vidéo</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-            </div>
-        </div>
-        <div class="accueil-cours-droite">
-            <div class="cours-contenu">
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-database">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-database"></use>
-                    </svg>
-                </a>
-                <div class="accueil-cours-texte">
-                    <h2>Web dynamique</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-            </div>
-        </div>
-        <div class="accueil-cours-gauche">
-            <div class="cours-contenu">
-                <div class="accueil-cours-texte">
-                    <h2>Design</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-font">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-font"></use>
-                    </svg>
-                </a>
-            </div>
-        </div>
-        <div class="accueil-cours-droite">
-            <div class="cours-contenu">
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-database">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-database"></use>
-                    </svg>
-                </a>
-                <div class="accueil-cours-texte">
-                    <h2>3D</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-            </div>
-        </div>
-        <div class="accueil-cours-gauche">
-            <div class="cours-contenu">
-                <div class="accueil-cours-texte">
-                    <h2>Gestion de projets</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-font">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-font"></use>
-                    </svg>
-                </a>
-            </div>
-        </div>
-        <div class="accueil-cours-droite">
-            <div class="cours-contenu">
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-unity">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-unity"></use>
-                    </svg>
-                </a>
-                <div class="accueil-cours-texte">
-                    <h2>Jeu-Vidéo</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-            </div>
-        </div>
-        <div class="accueil-cours-gauche">
-            <div class="cours-contenu">
-                <div class="accueil-cours-texte">
-                    <h2>Wordpress</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate ex suscipit fugiat porro, ea quasi perspiciatis natus quibusdam officia.</p>
-                </div>
-                <a href="https://eddym12.sg-host.com/cours">
-                    <svg class="icon icon-wordpress">
-                        <use xlink:href="wp-content/uploads/2021/icones.svg#icon-wordpress"></use>
-                    </svg>
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-
--->
-
-<div class="accueil-cours">
-    <?php if ( have_posts() ) : ?>
-        
-        
             <?php
             /* Start the Loop */
             
@@ -156,12 +38,10 @@
                     </div>
 
                 
-        
-    <?php endif; ?>
 
-    <img class="bg-cours-1" src="wp-content/uploads/bg-cours-1.svg"></img>
-    <img class="bg-cours-2" src="wp-content/uploads/bg-cours-2.svg"></img>
-</div>
+    
+    
+
 
 
 
