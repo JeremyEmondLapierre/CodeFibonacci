baa3d2e6740ccdc96da6940b9b63fad41b7da458
Louis Nicolas <shinoclabl@gmail.com>
2021-10-29 18:35:56 -0400

    mise en page du footer -logo footer et txt info site sass footer et footer.php

--- theme-codeFibonacci/footer.php
+++ theme-codeFibonacci/footer.php
@@ -12,32 +12,53 @@
  /*OWO*/ 
 ?>
 
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer">
 
-	<img class="bg-footer" src="wp-content/uploads/back-footer.svg"></img>
+	  <img class="bg-footer" src="wp-content/uploads/back-footer.svg"></img>
 
-		<div class="site-info">
+ <div class="global-footer">
+
+	 <img id="footer-logo" src="wp-content/uploads/footer-Logo.svg"></img>
+
+	 <div class="site-info">
+
+     <h3 id="site-info-titre">Site Info</h3>
+
+	    <p>Campus principal</p>
+	    <p>3800, rue Sherbrooke Est</p>
+	    <p>Montréal (Québec) H1X 2A2</p>
+	    <p>Tél.: 514 254-7131</p>
+	    <p>communic@cmaisonneuve.qc.ca</p>
 				
-		</div><!-- .site-info -->
+	 </div><!-- .site-info -->
 
 
-	<div class="liens-rapide">
+	 <div class="liens-rapide">
 
+	    
 	
-	</div>
+	 </div>
 
-		<div id="footer-tag">
+ </div>
+
+
+	 <div id="footer-tag">
 
 		
-			<a>Site départemental du TIM © 2021. Tout droits réservés</a>
+		<div id="div-texte-footer">Site départemental du TIM © 2021. Tout droits réservés</div>
 
-			<a>Conception et intégration du site par <?php
+		 
+
+		<div id="div-texte-logo"> <img id="logo-FiboFooter" src="wp-content/uploads/petit-logo-Fibonacci.svg"></img>
+
+			Conception et intégration du site par <?php
 				/* translators: 1: Theme name, 2: Theme author. */
 				printf( esc_html__( 'Thème: %1$s', 'equipe-5' ), 'Code Fibonacci.');
-				?></a>
+				?></div>
+	 </div>
 
-		
-		</div>
+ 
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
