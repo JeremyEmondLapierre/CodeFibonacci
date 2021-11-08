b2dfe846a9ec328dff3555d626c23eea40107ade
JeremyEmondLapierre <72357541+JeremyEmondLapierre@users.noreply.github.com>
2021-11-08 10:16:34 -0500

    Modification des noms des fichiers

--- theme-codeFibonacci/category-Stage.php
+++ theme-codeFibonacci/category-Stage.php
@@ -19,6 +19,7 @@ get_header();
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
+				<h1>Stage</h1>
 			</header><!-- .page-header -->
 
 			<?php
