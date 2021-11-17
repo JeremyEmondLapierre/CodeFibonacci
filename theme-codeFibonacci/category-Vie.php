babf7fa69ab5f40e2dc51dc234ef42a6598d1a5b
Louis Nicolas <shinoclabl@gmail.com>
2021-11-17 16:55:43 -0500

    grid projets

--- theme-codeFibonacci/category-Vie.php
+++ theme-codeFibonacci/category-Vie.php
@@ -11,27 +11,62 @@ get_header();
 ?>
 	
 	<main id="primary" class="site-main integre">
-
-	
+		<div>
 	        <h1>Vie Étudiante</h1>
-			<header class="page-header">
-				<div>
-				
-				<h4>Accueil</h4>
-				<h4>Vie Étudiante</h4>
-				</div>
-				
-			</header><!-- .page-header -->
+			<!--h4>Accueil</h4-->
+			<!--h4>Vie Étudiante</h4-->
+		</div>
+			
 
 			<section class="gallerie-projets">
 
 			<h3>Projets Étudiants</h3> 
 
+			  
+
 			  <div class="lesProjets">
 
-              <div class="bottom-box"></div>
-              <div class="top-box"></div>
-              
+
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+
+					<div class="top-midle" >
+
+						    <!--img ici-->
+							<!--img ici-->		
+				    </div>
+
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+
+
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+
+					<div class="midle" >
+						
+					        <!--img ici-->
+							<!--img ici-->
+				    </div>
+
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					
+					<div class="bottom" >
+						
+							<!--img ici-->
+							<!--img ici-->
+				    </div>
+					
+					
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					<div ><div><img class="logo-concours" src='<?php echo site_url().'/wp-content/uploads/2021/11/img-defaut.png';?>';></div></div>
+					
+					
+			  </div>
+
+
 			  </div>
 
 
@@ -51,6 +86,7 @@ get_header();
 			    <h4>Game Jam</h4>
 
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti necessitatibus provident error, eos aut impedit magni quae tenetur vero quis soluta, rerum blanditiis neque quas ratione expedita cumque dolorem?</p>
+				<p class="suite">Lire la suite...</p>
 
 			  </div>
 
@@ -61,7 +97,7 @@ get_header();
 			    <h4>Jour de la terre</h4>
 
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est corrupti, illo officia odit laboriosam placeat ea natus ullam, vitae provident nesciunt sunt! Neque reiciendis ut deleniti veniam necessitatibus quaerat reprehenderit?</p>
-
+				<p class="suite">Lire la suite...</p>
 			  </div>
 
 			</div>
