178746ddc54e69e2855c3ecae6187ed1cd9f8696
JeremyEmondLapierre <justkoly@gmail.com>
2021-11-19 21:24:27 -0500

    Modification de la page enseignants pour la rendre dynamique

--- theme-codeFibonacci/category-Enseignants.php
+++ theme-codeFibonacci/category-Enseignants.php
@@ -9,7 +9,6 @@
 
 get_header();
 ?>
-	//PAGE ENSEIGNANTS
 	<main id="primary" class="site-main integre">
 
 <div class="logique">
@@ -19,86 +18,27 @@ get_header();
 <div class="accordion-body" id="logique">
 <h2>Aspect <a>Logique</a></h2>
 
-<div class="dropdown">
-  <button class="dropbtn" id="boutonLogique">Filtrer par : <a>Logique</a></button>
-  <div class="dropdown-content">
-    <button id="boutonCreatif" class="">Créatif</button>  
-  </div>
-</div>
+	<div class="dropdown">
+	<button class="dropbtn" id="boutonLogique">Filtrer par : <a>Logique</a></button>
+		<div class="dropdown-content">
+			<button id="boutonCreatif" class="">Créatif</button>  
+		</div>
+	</div>
+
 
   <div class="accordion">
+  	
     
-
-    <div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/8.png';?>';><a>Martin Eddy</a></div>
-      <div class="content">études en mathématiques informatiques (B.Sc) et une maîtrise en traitement d'images à l'UQAM m'ont amené à m'intéresser plus particulièrement à la programmation Web.<br>
-	  <br>
-J'ai côtoyé l'industrie pendant plusieurs années comme programmeur analyste pour finalement trouvé ma vocation en enseignement au Collège de maisonneuve.<br>
-<br>
-Les sujets qui m'intéressent touchent directement la création de sites Web adaptatifs du côté client et serveur.<br>
-<br>
-J'enseigne au département TIM depuis 2001.</div>
-    </div>
- 
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/3.png';?>';><a>Caroline Martin</a></div>
-      <div class="content">On me traite de Geek car je programme en PHP depuis pratiquement sa création et bien avant sa réelle utilisation dans les sites Web.<br>
-	  <br>
-	  	  J'aime, voire, je suis passionnée par la programmation informatique.<br>
-	  <br> 
-		  Je joue à D&D mais je ne suis pas que cela !</div>
-    </div>
-
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/1.png';?>';><a> Camille Semaan</a></div>
-      <div class="content">Ma vocation : communiquer connaissances et savoir-faire.<br><br>
-                  Ma devise : «Comprendre et ne pas juger» (Georges Simenon)<br>
-				  <br> 
-				  On a tout notre temps.
-    </div>
-    </div>
-
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/12.png';?>';><a>Mathieu Dionne</a></div>
-      <div class="content">
-	  Prof « Made in Maisonneuve ! » <br>
-	  Je hante ces corridors depuis si longtemps… Eh oui…j’ai fait mes études collégiales ici même ! <br>
-	  <br>
-	  J’y suis revenu après mes études universitaires à l’UQAM, en communication, concentration multimédia interactive.<br>
-	  <br> 
-	  J’enseigne la création de jeux vidéo et l’environnement sonore aux étudiants du programme TIM depuis 2002. Du point de vue professionnel, mes réalisations incluent notamment la mise au point du cours «Développement de l’enfant et de l’adolescent», pour le compte du Cégep @ distance, qui a remporté le prix de l’excellence en conception pédagogique de l’ACED en 2003, le site Web de Kid A, un groupe hommage à Radiohead, et bien d’autres encore.<br>
-	  </div>
-    </div>
-
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/6.png';?>';><a>Vagharshak 'Vahik' Toroussian</a></div>
-      <div class="content">
-	  J’enseigne au Collège de Maisonneuve depuis 10 ans où j’occupe d’ailleurs le rôle de coordonnateur du programme des Techniques d’intégration multimédia.<br>
-	  <br>
-	  J’ai fait mes études à l’Université de Montréal et obtenu une maîtrise en traitement de l’image. Ma spécialité est en programmation sur support et j’entends vous faire apprécier l’incroyable potentiel de ce volet multimédia.<br>
-	  <br> 
-	  Ce qui vous attend : des travaux pratiques sous forme de jeux (casse-tête, quêtes, etc.) et la création de prototypes de jeux avec Virtools. Message à ceux qui aiment bien équilibrer travail et activité physique : je suis toujours disponible pour une partie de soccer… à bon entendeur salut!
-	  </div>
-    </div>
-
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/10.png';?>';><a>Johanne Massé</a></div>
-      <div class="content">
-	  Je suis gestionnaire de projet et architecte-paysagiste de formation mais, paradoxalement, passionnée depuis toujours par les 0 et les 1... J'ai cofondé à la fin des années 80 une entreprise spécialisée dans le développement de logiciels pour l'aménagement du territoire.<br>
-	  <br> 
-	  À l'aube du nouveau millénaire, j'ai fait le grand saut et réorienté ma carrière en multimédia. J’ai fait mes débuts à titre de consultante et j'exerce la profession d'enseignante depuis 2001. Vous devinerez par la maxime d'introduction que j'ai un petit faible pour la gestion de projets multimédias.<br>
-	  <br> 
-	  Mais ce n'est pas mon seul dada, je me passionne aussi pour les logiciels-auteurs et la programmation, notamment Flash et ActionScript. Parallèlement à mes activités d'enseignante, je poursuis également une maîtrise en pédagogie.
-	  </div>
-    </div>
-
-	<div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/7.png';?>';><a>Vincent Leblanc</a></div>
-      <div class="content">
-	  Mon séjour au Collège de Maisonneuve a débuté à l’automne 2001. J’ai donné mes premiers cours au Département TIM à l’hiver 2002. J’ai été formé à l’École de Technologie Supérieure (génie), à l’Université de Montréal (communication) et à l’Université Bishops (arts visuels).<br><br>
-	  Ces dernières années, j’ai surtout donné les cours de création Web de 1ère année du programme de TIM , ainsi que le cours Production artistique et arts médiatiques, en 3ème session du  programme Science, Lettres et Arts. Je suis aussi un des représentants à la coordination départementale depuis la session d’automne 2018. Durant mes temps libres, j’aime faire plusieurs choses, mais je préfère par-dessus tout : dormir!</option>
-	  </div>
-    </div>
+	<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				if(get_field('aspect') == "Logique"):
+					get_template_part( 'template-parts/content', 'Enseignants' );
+				endif;
+				endwhile;
+		?>	
+    
 
   </div>
   </div>
@@ -106,61 +46,29 @@ J'enseigne au département TIM depuis 2001.</div>
 
   <div class="accordion-body-pasActif" id="creatif">
   <h2>Aspect <a>creatif</a></h2>
-  
+  <div class="dropdown">
+  <button class="dropbtn" id="boutonCreatif">Filtrer par : <a>Créatif</a></button>
+  <div class="dropdown-content">
+    <button id="boutonLogique" class="">Logique</button>  
+  </div>
 </div>
+  
+
 
   <div class="accordion">
-
-  <div class="container">
-      <div class="label"><img class="img-prof" src='<?php echo site_url().'/wp-content/uploads/2021/12/8.png';?>';><a>Martin Eddy2</a></div>
-      <div class="content">études en mathématiques informatiques (B.Sc) et une maîtrise en traitement d'images à l'UQAM m'ont amené à m'intéresser plus particulièrement à la programmation Web.<br>
-	  <br>
-J'ai côtoyé l'industrie pendant plusieurs années comme programmeur analyste pour finalement trouvé ma vocation en enseignement au Collège de maisonneuve.<br>
-<br>
-Les sujets qui m'intéressent touchent directement la création de sites Web adaptatifs du côté client et serveur.<br>
-<br>
-J'enseigne au département TIM depuis 2001.</div>
-    </div>
-
-  </div>
-  </div>
-
-
-
-
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
+			if(get_field('aspect') == "Creatif"):
+				get_template_part( 'template-parts/content', 'Enseignants' );
+			endif;
 			endwhile;
+	?>
 
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		
-		?>
+  </div>
+</div>
+</div>
 
 	</main><!-- #main -->
 
