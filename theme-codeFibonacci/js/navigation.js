094ce923ffc02e4f5e125a116a2cfbfc02b5a29e
Sami El Arif <sami.elarif@hotmail.com>
2021-11-29 10:20:36 -0500

    Amélioration carrousel Vie étudiante pt.1

--- theme-codeFibonacci/js/navigation.js
+++ theme-codeFibonacci/js/navigation.js
@@ -98,46 +98,6 @@
 	}
 }() );
 
-/*--------------------------------------------
-  Le carrousel de la Vie étudiante
-----------------------------------------------*/
-
-swiperVie = new Swiper('.accueil-vie', {
-	// Optional parameters
-	direction: 'horizontal',
-	loop: true,
-	spaceBetween: 20,
-	centeredSlides : true,
-	speed: 700,
-  
-	pagination: {
-	  el: '.accueil-vie .swiper-pagination',
-	  type: 'bullets',
-	  clickable: true,
-	},
-  
-	// Les boutons de navigation
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-
-	breakpoints: {
-		// when window width is >= 600px
-		600: {
-			slidesPerView: 1,
-		},
-		// when window width is >= 800px
-		800: {
-			slidesPerView: 3,
-		},
-		// when window width is >= 1200px
-		1200: {
-			slidesPerView: 3,
-		}
-	},
-  
-});
 
 
 /*--------------------------------------------
