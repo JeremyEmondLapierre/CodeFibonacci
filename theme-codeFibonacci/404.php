2a57660bc9094c893b719244359ab942116daa4b
Sami El Arif <sami.elarif@hotmail.com>
2021-11-29 10:58:06 -0500

    Intégration HTML Page 404 pt.1

--- theme-codeFibonacci/404.php
+++ theme-codeFibonacci/404.php
@@ -10,47 +10,17 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main integre">
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'equipe-5' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Page 404', 'equipe-5' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'equipe-5' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'equipe-5' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$equipe_5_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'equipe-5' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$equipe_5_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
+				<h3>Cette page est malheureusement indisponible</h3>
+				<p>Le lien que vous tentez de suivre est probablement rompu, supprimé ou inexistant.</p>
+				<img src="<?php echo site_url().'/wp-content/uploads/2021/11/icone-404;?>" alt="">
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
 
