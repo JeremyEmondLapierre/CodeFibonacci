39efb878e3d6e717b4e40ea54d2f8e211411e962
Sami El Arif <sami.elarif@hotmail.com>
2021-12-02 10:34:04 -0500

    Correction images de fond Accueil

--- theme-codeFibonacci/front-page.php
+++ theme-codeFibonacci/front-page.php
@@ -16,7 +16,7 @@ get_header();
 ?>
 
 
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main" style="background-image: url('<?php echo site_url().'/wp-content/uploads/2021/11/accueil-bg-2.png';?>');">
 
 		<?php
 		if ( have_posts() ) :
@@ -39,7 +39,7 @@ get_header();
 		
 		?>
 
-		<div id="accueil-prof">
+		<div id="accueil-prof" >
     		<h1>Qu'est ce que le <strong>TIM</strong> ?</h1>
     		<p>Suivez le courant du multimédia en vous inscrivant au TIM du Collège de Maisonneuve ! Laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
 
@@ -66,7 +66,7 @@ get_header();
 		</div>
 			
 		
-		<div class="accueil-cours" style="background-image: url('<?php echo site_url().'/wp-content/uploads/2021/11/accueil-bg-2.png';?>');">
+		<div class="accueil-cours" style="background-image: url('<?php echo site_url().'/wp-content/uploads/2021/11/accueil-bg-3.png';?>');">
 			<h1>Nos cours</h1>
 			<section class="accueil-cours-contenant">
 				<?php
@@ -82,7 +82,7 @@ get_header();
 		</div>
 
 		<!-- Le contant du swiper au complet -->
-		<div class="accueil-vie swiper" style="background-image: url('<?php echo site_url().'/wp-content/uploads/2021/11/accueil-bg-3.png';?>');">
+		<div class="accueil-vie swiper" >
 			<h1>Vie étudiante</h1>
 			
 			<!-- Le contenant du carrousel des images UNIQUEMENT, pas de ses composantes -->
